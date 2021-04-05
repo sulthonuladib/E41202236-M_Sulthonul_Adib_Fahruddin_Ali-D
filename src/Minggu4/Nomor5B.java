aae8a87737a76a6f903e2de35d6ca66a9031a1dc
Sulthonul Adib <57745493+sulthonuladib@users.noreply.github.com>
2021-04-05 19:30:20 +0700

    Done: late, done week 4 5b

---
+++ src/Minggu4/Nomor5B.java
@@ -0,0 +1,16 @@
+package Minggu4;
+
+public class Nomor5B {
+    public static void main(String[] args) {
+        System.out.println("\t Do_While");
+        System.out.println("Bilangan kelipatan 2 (1-100)");
+        System.out.println("============================");
+        int i = 1;
+
+        do {
+            System.out.print( i +" ");
+            i = i * 2;
+        } while (i <= 100 );
+
+    }
+}
