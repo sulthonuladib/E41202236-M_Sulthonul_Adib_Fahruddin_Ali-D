6014c4a20a89605262e8c3b69705e4cb5896c738
Sulthonul Adib <57745493+sulthonuladib@users.noreply.github.com>
2021-04-10 18:58:50 +0700

    Add: GraphApp main Class

---
+++ src/Minggu5/Graph/GraphApp.java
@@ -0,0 +1,20 @@
+package Minggu5.Graph;
+
+public class GraphApp {
+    public static void main(String[] args) {
+        Graph theGraph = new Graph();
+
+        theGraph.addVertex('A');
+        theGraph.addVertex('B');
+        theGraph.addVertex('C');
+        theGraph.addVertex('D');
+        theGraph.addVertex('E');
+
+        theGraph.addEdge(0, 1);
+        theGraph.addEdge(1, 2);
+        theGraph.addEdge(2, 3);
+        theGraph.addEdge(3, 4);
+
+        theGraph.display();
+    }
+}
