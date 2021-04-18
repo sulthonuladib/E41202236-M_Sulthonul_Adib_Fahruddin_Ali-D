69837328609b03ea3e247d76fdaf2ce0993abc88
Sulthonul Adib <sulthonuladib@github.com>
2021-04-18 21:58:53 +0700

    Done: Week 6 BinarySearch and SequentialSearch

---
+++ src/Minggu6/BubbleSort.java
@@ -0,0 +1,39 @@
+// package Minggu6;
+
+
+class BubbleSort {
+    static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        int temp = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 1; j < (n-1); j++) {
+
+                if (arr[j-1] > arr[j]){
+                    temp = arr[j-1];
+                    arr[j-1] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+    }
+
+    public static void main (String[] args) {
+        int arr[] = {
+            3, 60, 35, 2, 45, 320, 5
+        };
+
+        System.out.println("Array before Bubble Sort");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+
+        System.out.println("Array After Bubble Sort");
+
+        bubbleSort(arr);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
